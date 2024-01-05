d9d28c510c0d0bf18f5335b63cbae30445143da0
Ed-Luan <67657729+Ed-Luan@users.noreply.github.com>
2024-01-05 18:13:07 +0000

    Update pages/index.tsx
    Signed-off-by: Ed-Luan <67657729+Ed-Luan@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,12 @@ import About from '../components/Sections/About';
 import Contact from '../components/Sections/Contact';
 import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
+import {homePageMeta} from '../data/data';
 import Page from '../components/Layout/Page';
 import Research from '../components/Sections/Research';
 import Resume from '../components/Sections/Resume';
 import Testimonials from '../components/Sections/Testimonials';
-import {homePageMeta} from '../data/data';
+
 
 // eslint-disable-next-line react-memo/require-memo
 const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
